2101e771b093cf2408d36851fb89e18862463d18
Miguel David González del Toro <migueldgdt@gmail.com>
2025-01-10 20:15:10 -0500

    build (BookingHistory): created basic entity

---
+++ Domain/Entities/BookingHistory.cs
@@ -0,0 +1,20 @@
+using ManagementSystem.Api.Common.Domain;
+
+namespace ManagementSystem.Api.Domain.Entities;
+
+public class BookingHistory : BaseEntity
+{
+    public Guid BookingId { get; private set; }
+    public Booking Booking { get; private set; } = null!;
+    public string Description { get; private set; } = null!;
+    
+    private BookingHistory() { } 
+    
+    public BookingHistory(Booking booking, string description)
+    {
+        BookingId = booking.Id;
+        Booking = booking;
+        Description = description;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
