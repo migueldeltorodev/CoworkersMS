0859cf558b617c3544ad9ca6bf0b71690b03cfb7
Miguel David González del Toro <migueldgdt@gmail.com>
2025-01-10 23:04:52 -0500

    build (BaseRepository): created basic repo implementation

---
+++ Common/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+using ManagementSystem.Api.Common.Domain;
+
+namespace ManagementSystem.Api.Common.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : BaseEntity
+{
+    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
+    IQueryable<TEntity> AsQueryable();
+}
---
+++ Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,51 @@
+using ManagementSystem.Api.Common.Domain;
+using ManagementSystem.Api.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagementSystem.Api.Persistence.Repositories;
+
+public abstract class BaseRepository<TEntity> : IRepository<TEntity> 
+    where TEntity : BaseEntity
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    protected BaseRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public virtual async Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _dbSet.FindAsync(new object[] { id }, cancellationToken);
+    }
+
+    public virtual async Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbSet.ToListAsync(cancellationToken);
+    }
+
+    public virtual async Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        await _dbSet.AddAsync(entity, cancellationToken);
+        return entity;
+    }
+
+    public virtual Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        return Task.CompletedTask;
+    }
+
+    public virtual Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
+    {
+        _dbSet.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public virtual IQueryable<TEntity> AsQueryable()
+    {
+        return _dbSet.AsQueryable();
+    }
+}
