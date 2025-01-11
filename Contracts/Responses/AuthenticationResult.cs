936fb2081dd24fa1f7983c4a62e521504bdc5210
Miguel David González del Toro <migueldgdt@gmail.com>
2025-01-10 23:59:36 -0500

    refactor (AuthenticationResult): deleted RefreshToken

--- Contracts/Responses/AuthenticationResult.cs
+++ Contracts/Responses/AuthenticationResult.cs
@@ -3,6 +3,5 @@ namespace ManagementSystem.Api.Contracts.Responses;
 public record AuthenticationResult
 {
     public string Token { get; init; } = null!;
-    public string RefreshToken { get; init; } = null!;
     public DateTime ExpiresAt { get; init; }
 }
