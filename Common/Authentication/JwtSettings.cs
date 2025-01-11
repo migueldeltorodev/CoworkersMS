5c3e4719172f53f4846f25f50b91e0b0054ae1b1
Miguel David González del Toro <migueldgdt@gmail.com>
2025-01-10 23:52:52 -0500

    build (JwtSettings): created class for Jwt configuration

---
+++ Common/Authentication/JwtSettings.cs
@@ -0,0 +1,10 @@
+namespace ManagementSystem.Api.Common.Authentication;
+
+public class JwtSettings
+{
+    public const string SectionName = "JwtSettings";
+    public string Secret { get; init; } = default!;
+    public int ExpiryInMinutes { get; init; } 
+    public string Issuer { get; init; } = default!;
+    public string Audience { get; init; } = default!;
+}
