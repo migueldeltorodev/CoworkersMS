a0ea9bbd9ec241a555d587c1cd5424342dd76017
Miguel David González del Toro <migueldgdt@gmail.com>
2025-01-12 10:57:56 -0500

    refactor (Program): added Swagger

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using ManagementSystem.Api.Common.Extensions;
+using ManagementSystem.Api.Endpoints.Booking;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -12,6 +14,13 @@ var builder = WebApplication.CreateBuilder(args);
     
     // Mapper profiles
     builder.Services.AddAutoMapper(typeof(Program));
+    
+    // Add Swagger services
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "ManagementSystem API", Version = "v1" });
+    });
 }
 
 var app = builder.Build();
@@ -19,6 +28,14 @@ var app = builder.Build();
     if (app.Environment.IsDevelopment())
     {
         app.MapOpenApi();
+        // Enable middleware to serve generated Swagger as a JSON endpoint.
+        app.UseSwagger();
+        // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+        // specifying the Swagger JSON endpoint.
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "ManagementSystem API V1");
+        });
     }
 
     app.UseHttpsRedirection();
