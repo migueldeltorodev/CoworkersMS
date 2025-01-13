6ec381135887d364541bffdc087e3681884bb37e
Miguel David González del Toro <migueldgdt@gmail.com>
2025-01-12 23:55:13 -0500

    refactor (AppDbContext): added SaveChangesAsync method

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -1,3 +1,4 @@
+using ManagementSystem.Api.Common.Domain;
 using ManagementSystem.Api.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,32 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+                    entry.Entity.CreatedBy = GetCurrentUser();
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+                    entry.Entity.UpdatedBy = GetCurrentUser();
+                    break;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private string GetCurrentUser()
+    {
+        // TODO: Implement with IHttpContextAccessor
+        return "system";
     }
     
 }
